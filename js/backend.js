7f9bc1227e07d80d18569deb6208ba1be4da7a44
Andrew Minion <andrew@andrewrminion.com>
2016-03-30 17:14:19 -0400

    remove jQuery.off in backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function(){
-    jQuery('[name="post_title"], #acf-field-address_1, #acf-field-address_2, #acf-field-city, #acf-field-state, #acf-field-zip').off('blur');
     jQuery('[name="post_title"], #acf-field-address_1, #acf-field-address_2, #acf-field-city, #acf-field-state, #acf-field-zip').on('blur', function(){
         var address = jQuery('[name="post_title"]').val() + ' ' + jQuery('#acf-field-address_1').val() + ' ' + jQuery('#acf-field-address_2').val() + ' ' + jQuery('#acf-field-city').val() + ', ' + jQuery('#acf-field-state').val() + ' ' + jQuery('#acf-field-zip').val();
         console.log('address to look up: ' + address);
